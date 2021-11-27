1e44889bc3a88bdf4c584b502e042620fcd1105a
vhorozheiev <vhorozheiev@gmail.com>
2021-11-27 10:46:20 +0200

    add first version of solution

---
+++ test/specs/example.e2e.js
@@ -0,0 +1,14 @@
+describe('My Login application', () => {
+    it('should login with valid credentials', async () => {
+        await browser.url(``);
+
+        await $('#username').setValue('tomsmith');
+        await $('#password').setValue('SuperSecretPassword!');
+        await $('button[type="submit"]').click();
+
+        await expect($('#flash')).toBeExisting();
+        await expect($('#flash')).toHaveTextContaining(
+            'You logged into a secure area!');
+    });
+});
+
